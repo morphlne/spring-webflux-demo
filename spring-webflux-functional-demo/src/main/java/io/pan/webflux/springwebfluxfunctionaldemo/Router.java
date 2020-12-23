3d55f519c4a1a0bd62a85994d7cf0b4bf1535ef7
morphlne <lsmrphln@gmail.com>
2020-12-23 22:26:07 +0300

    Json example

---
+++ spring-webflux-functional-demo/src/main/java/io/pan/webflux/springwebfluxfunctionaldemo/Router.java
@@ -0,0 +1,41 @@
+package io.pan.webflux.springwebfluxfunctionaldemo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.util.function.Supplier;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+public class Router implements Supplier<RouterFunction<ServerResponse>> {
+
+  private final HandlerFunction<ServerResponse> okString;
+  private final HandlerFunction<ServerResponse> okJson;
+
+  public Router(HandlerFunction<ServerResponse> okString, HandlerFunction<ServerResponse> okJson) {
+    this.okString = okString;
+    this.okJson = okJson;
+  }
+
+  public Router() {
+    this(
+        request -> ServerResponse.ok().body(fromValue("Hello")),
+        request -> ServerResponse.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(fromValue(new Greeting("Hello")))
+    );
+  }
+
+  @Override
+  public RouterFunction<ServerResponse> get() {
+    return route(
+        GET("/"), okString
+    ).andRoute(
+        GET("/json"), okJson
+    );
+  }
+}
--- spring-webflux-functional-demo/src/main/java/io/pan/webflux/springwebfluxfunctionaldemo/SpringWebfluxFunctionalDemo.java
+++ spring-webflux-functional-demo/src/main/java/io/pan/webflux/springwebfluxfunctionaldemo/SpringWebfluxFunctionalDemo.java
@@ -1,31 +1,18 @@
 package io.pan.webflux.springwebfluxfunctionaldemo;
 
-import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.reactive.function.server.HandlerFunction;
-import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.netty.http.server.HttpServer;
-
-import static org.springframework.web.reactive.function.BodyInserters.fromValue;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 public class SpringWebfluxFunctionalDemo {
 
   public static void main(String[] args) throws InterruptedException {
 
-    HandlerFunction<ServerResponse> okResponse = request -> ServerResponse.ok().body(fromValue("Hello"));
-
-    RouterFunction<ServerResponse> router = route(GET("/"), okResponse);
-
-    HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);
-
     HttpServer
         .create().host("localhost").port(8080)
-        .handle(new ReactorHttpHandlerAdapter(httpHandler))
-        .bindNow();
+        .handle(new ReactorHttpHandlerAdapter(
+            RouterFunctions.toHttpHandler(new Router().get()))
+        ).bindNow();
 
     Thread.currentThread().join();
   }
